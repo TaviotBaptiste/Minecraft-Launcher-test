59d3a973f3e05dc56b030f5871ccf305c897d134
TaviotBaptiste <Xildrite@yahoo.fr>
2023-04-24 14:34:17 +0200

    add background + style.css

--- src/main/java/com/github/taviotbaptiste/launcher/MainApplication.java
+++ src/main/java/com/github/taviotbaptiste/launcher/MainApplication.java
@@ -4,27 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
+import java.io.Console;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LauncherController.class.getResource("launcher-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 580);
 
+        //Add name of the launcher
         stage.setTitle("Sensta");
+        //Add background
+        scene.getStylesheets().add(String.valueOf(this.getClass().getResource("style.css")));
+        //Min Width/Height
         stage.setMinWidth(720);
         stage.setMinHeight(580);
+        //Max width/Height
         stage.setMaxWidth(800);
         stage.setMaxHeight(650);
-        stage.centerOnScreen();
+
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+        System.out.println();
     }
 }
