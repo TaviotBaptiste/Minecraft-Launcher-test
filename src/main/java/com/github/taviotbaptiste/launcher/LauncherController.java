3acf92578e4638d85de4fbd19f1a57d97aa1565c
TaviotBaptiste <Xildrite@yahoo.fr>
2023-04-24 16:51:21 +0200

    feat: add center; add event update label after click

--- src/main/java/com/github/taviotbaptiste/launcher/LauncherController.java
+++ src/main/java/com/github/taviotbaptiste/launcher/LauncherController.java
@@ -1,14 +1,18 @@
 package com.github.taviotbaptiste.launcher;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 
 public class LauncherController {
+
     @FXML
     private Label playText;
 
     @FXML
     protected void onPlayButtonClick() {
         playText.setText("Jouer");
+        playText.setAlignment(Pos.CENTER);
     }
+
 }
--- src/main/java/com/github/taviotbaptiste/launcher/MainApplication.java
+++ src/main/java/com/github/taviotbaptiste/launcher/MainApplication.java
@@ -3,27 +3,34 @@ package com.github.taviotbaptiste.launcher;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.io.Console;
 import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LauncherController.class.getResource("launcher-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720, 580);
+        FXMLLoader root = new FXMLLoader(LauncherController.class.getResource("launcher-view.fxml"));
+        Scene scene = new Scene(root.load(), 500, 500);
 
         //Add name of the launcher
         stage.setTitle("Sensta");
         //Add background
         scene.getStylesheets().add(String.valueOf(this.getClass().getResource("style.css")));
         //Min Width/Height
-        stage.setMinWidth(720);
-        stage.setMinHeight(580);
+        stage.setMinWidth(500);
+        stage.setMinHeight(500);
         //Max width/Height
-        stage.setMaxWidth(800);
-        stage.setMaxHeight(650);
+        stage.setMaxWidth(500);
+        stage.setMaxHeight(500);
+
+
+
+        //For transparent background (+style.css)
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
 
         stage.setScene(scene);
         stage.show();
@@ -31,6 +38,5 @@ public class MainApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-        System.out.println();
     }
 }
