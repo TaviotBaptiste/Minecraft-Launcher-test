2716536978397e19e7e76107c193e5b0bfe304c9
TaviotBaptiste <Xildrite@yahoo.fr>
2023-04-25 11:17:47 +0200

    feat: Better show

--- src/main/java/com/github/taviotbaptiste/launcher/LauncherController.java
+++ src/main/java/com/github/taviotbaptiste/launcher/LauncherController.java
@@ -1,18 +1,34 @@
 package com.github.taviotbaptiste.launcher;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LauncherController {
-
     @FXML
     private Label playText;
+    @FXML
+    private Button playButton;
 
     @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+
+    @FXML
+    public void initialize() {
+        username.setPromptText("email");
+        password.setPromptText("Mot de passe");
+        username.getParent().requestFocus();
+        username.setFocusTraversable(false);
+        password.getParent().requestFocus();
+        password.setFocusTraversable(false);
+        playButton.setBackground(null);
+    }
+    @FXML
     protected void onPlayButtonClick() {
-        playText.setText("Jouer");
-        playText.setAlignment(Pos.CENTER);
+        playText.setText("Connexion");
     }
 
 }
